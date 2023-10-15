7a56d9fd508ed60ee40ac3b34448190531278b55
George Emetu <g.emetu11@gmail.com>
2023-10-15 19:35:02 +0100

    done? with task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -14,14 +14,12 @@ def do_pack():
     return the archive path if archive has generated correctly.
     """
     local("mkdir -p versions")
-    date = datetime.now().strftime("%Y%m%d%H%M%S")
-    archived_f_path = "versions/web_static_{}.tgz".format(date)
-    t_gzip_archive = local("tar -cvzf {} web_static".format(archived_f_path))
-
-    if t_gzip_archive.succeeded:
-        return archived_f_path
-    else:
+    path = ("versions/web_static_{}.tgz"
+            .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
+    result = local("tar -cvzf {} web_static".format(path))
+    if result.failed:
         return None
+    return path
 
 def do_deploy(archive_path):
     """
