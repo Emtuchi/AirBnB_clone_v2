7a6c4b44e200014ca5625bc4c5bafcfbcaa7b14d
George Emetu <g.emetu11@gmail.com>
2023-10-20 18:17:16 +0100

    task 1

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,12 +6,12 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_flask():
+@app.route('/', strict_slashes = False)
+def hello_HBNB():
     """Return string when route queried
+       using curl
     """
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
-    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
